be7adf290ba2a33cb508adcc36363312618c792a
rootscope <datrootscope@gmail.com>
2014-12-11 23:42:37 -0700

    init count_x

---
+++ count_x.cpp
@@ -0,0 +1,10 @@
+int count_x(char∗ p, char x){
+  // count the number of occurrences of x in p[]
+  // p is assumed to point to a zero-ter minated array of char (or to nothing)
+  if(p==nullptr) return 0;
+    int count = 0;
+    for(; ∗p!=0; ++p)
+      if(∗p == x)
+        ++count;
+    return count;
+}
